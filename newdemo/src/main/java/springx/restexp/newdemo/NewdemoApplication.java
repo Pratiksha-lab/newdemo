33ceaae746c56aa85864f96655af4eff14e933a8
pratiksha patil <pratikshaspatil1905@gmail.com>
2025-02-12 19:12:56 +0530

    first commit

---
+++ newdemo/src/main/java/springx/restexp/newdemo/NewdemoApplication.java
@@ -0,0 +1,15 @@
+package springx.restexp.newdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewdemoApplication.class, args);
+		
+		System.out.println("hello world");
+	}
+
+}
