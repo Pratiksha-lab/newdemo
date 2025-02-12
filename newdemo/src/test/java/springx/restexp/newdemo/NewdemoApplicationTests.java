2a812dddd7bff68c6b77fca320a888c6c49785d9
pratiksha patil <pratikshaspatil1905@gmail.com>
2025-02-12 19:07:27 +0530

    first commit

---
+++ newdemo/src/test/java/springx/restexp/newdemo/NewdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package springx.restexp.newdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
